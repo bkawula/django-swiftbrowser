f5c36b7ac0cdc4789e9ef70f97e7ef8d120eb64d
bartek kawula <b.kawula@gmail.com>
2014-04-03 15:51:30 -0400

    settings update

--- README.md
+++ README.md
@@ -1,22 +1,20 @@
 django-swiftbrowser
 ===================
 
-[![Build Status](https://travis-ci.org/cschwede/django-swiftbrowser.png?branch=master)](https://travis-ci.org/cschwede/django-swiftbrowser)
+This is a fork of [cschwede / django-swiftbrowser](https://github.com/cschwede/django-swiftbrowser) that uses [django-storage-swift](https://github.com/wecreatepixels/django-storage-swift) instead of the formpost middleware.
 
 Simple web app build with Django and Twitter Bootstrap to access Openstack Swift.
 
-* No database needed
 * Works with keystone, tempauth & swauth
 * Support for public containers. ACL support in the works
 * Minimal interface, usable on your desktop as well as on your smartphone
-* Screenshots anyone? See below!
 
 Quick Install
 -------------
 
 1) Install swiftbrowser:
 
-    git clone git://github.com/cschwede/django-swiftbrowser.git
+    git clone git://github.com/bkawula/django-swiftbrowser.git
     cd django-swiftbrowser
     sudo python setup.py install
 
@@ -29,7 +27,7 @@ Quick Install
     django-admin.py startproject myproj
     cd myproj
     cp ~/django-swiftbrowser/example/settings.py myproj/settings.py
-
+	mkdir myproj/database
 
 3) Adopt myproj/settings.py to your needs, especially settings for Swift and static file directories.
 
@@ -46,31 +44,9 @@ Quick Install
     python manage.py collectstatic
 
 6) Run development server:
-    
+   
     python manage.py runserver
 
    *Important*: Either use 'python manage.py runserver --insecure' or set DEBUG = True in myproj/settings.py if you want to use the
    local development server. Don't use these settings in production!
 
-7) Use 'account:username' to login (or tenant/project:username if using Keystone).
-
-8) Deploying to production? Have a look at Djangos docs: https://docs.djangoproject.com/en/1.5/howto/deployment/wsgi/
-
-9) Please make sure that "tempurl" and "formpost" middlewares are activated in your proxy server. Extract from /etc/swift/proxy-server.conf:
-
-    [pipeline:main]
-    pipeline = catch_errors gatekeeper healthcheck proxy-logging cache tempurl formpost tempauth proxy-logging proxy-server
-
-    [filter:tempurl]
-    use = egg:swift#tempurl
-
-    [filter:formpost]
-    use = egg:swift#formpost
-
-
-Screenshots
------------
-
-![Login screen](screenshots/00.png)
-![Container view](screenshots/01.png)
-![Object view](screenshots/02.png)
--- example/settings.py
+++ example/settings.py
@@ -1,12 +1,18 @@
 """ Settings for Django project """
 import os
 
-# The database isn't used, but Django might complain if setting missing
-DATABASES = { 
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
+TEMPLATE_DIRS = ( os.path.join(PROJECT_PATH, 'templates'),)
+
+DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'swiftbrowser.db'
-    }   
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(PROJECT_PATH, 'database/database.sqlite3'),                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
 }
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
@@ -14,18 +20,15 @@ SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
 USE_L10N = True
 USE_TZ = True
 
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-)
-
-PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
-TEMPLATE_DIRS = ( os.path.join(PROJECT_PATH, 'templates'),)
+# Media folder isn't used, but required by models
+MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media')
+MEDIA_URL = '/media/'
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.middleware.http.ConditionalGetMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
 )
@@ -33,22 +36,76 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'myproj.urls'
 
 INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.staticfiles',
-    'swiftbrowser', 
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'swiftbrowser',
+    'jfu',
+)
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+   
+)
+TEMPLATE_CONTEXT_PROCESSORS = {
+    'django.core.context_processors.request',
+    'django.core.context_processors.static',
+}
+FILE_UPLOAD_HANDLERS = (
+    'progressbarupload.uploadhandler.ProgressBarUploadHandler',
+    'django.core.files.uploadhandler.MemoryFileUploadHandler',
+    'django.core.files.uploadhandler.TemporaryFileUploadHandler',    
 )
 
-SWIFT_AUTH_URL = 'http://127.0.0.1:8080/auth/v1.0'
-SWIFT_AUTH_VERSION = 1 # 2 for keystone
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
+DEFAULT_FILE_STORAGE='swift.storage.SwiftStorage'
+
+SWIFT_AUTH_URL = 'http://127.0.0.1:8080/auth/v2.0/'
+SWIFT_AUTH_VERSION = 2 # 2 for keystone
 STORAGE_URL = 'http://127.0.0.1:8080/v1/'
 BASE_URL = 'http://127.0.0.1'
 SWAUTH_URL = 'http://127.0.0.1:8080/auth/v2'
 STATIC_DIR = '/var/www/myproj/static'
 
-TIME_ZONE = 'Europe/Berlin'
-LANGUAGE_CODE = 'de-de'
+TIME_ZONE = 'America/Toronto'
+LANGUAGE_CODE = 'en-en'
 SECRET_KEY = 'DONT_USE_THIS_IN_PRODUCTION'
 STATIC_URL = "/static/"
 STATIC_ROOT = "/var/www/myproj/static/"
 
+# For django-storage-swift. 
+SWIFT_USERNAME = 'username'
+SWIFT_TENANT_NAME = 'TENANT'
+SWIFT_BASE_URL = BASE_URL
+SWIFT_KEY = 'yourkey'
+SWIFT_CONTAINER_NAME = '/'
+
 ALLOWED_HOSTS = ['127.0.0.1', 'insert_your_hostname_here']
+
+DEBUG = True
