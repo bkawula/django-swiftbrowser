4d435717d8dff5b058b920f890ba5fd1eecc4fd1
Peter Tran <petah.tran@utoronto.ca>
2015-04-23 11:22:14 -0400

    Ignored build files and pep8d urls.py

--- swiftbrowser/urls.py
+++ swiftbrowser/urls.py
@@ -10,7 +10,8 @@ from swiftbrowser.views import containerview, objectview, download,\
     restore_trash, move_collection_to_trash, restore_trash_collection,\
     download_collection
 
-urlpatterns = patterns('swiftbrowser.views',
+urlpatterns = patterns(
+    'swiftbrowser.views',
     url(r'^login/$', login, name="login"),
     url(r'^$', containerview, name="containerview"),
     url(r'^public/(?P<account>.+?)/(?P<container>.+?)/(?P<prefix>(.+)+)?$',
@@ -31,27 +32,41 @@ urlpatterns = patterns('swiftbrowser.views',
     url(r'^objects/(?P<container>.+?)/(?P<prefix>(.+)+)?$', objectview,
         name="objectview"),
     url(r'^object_table/', objecttable, name="objecttable"),
-    url( r'upload/', upload, name = 'jfu_upload' ),
+    url(r'upload/', upload, name='jfu_upload'),
     url(r'^acls/(?P<container>.+?)/$', edit_acl, name="edit_acl"),
-    url( r'^delete/(?P<pk>.+)$', upload_delete, name = 'jfu_delete' ),
-    url(r'^thumbnails/(?P<container>.+?)/(?P<objectname>.+?)$',
-            serve_thumbnail, name="serve_thumbnail"),
-    url(r'^move_to_trash/(?P<container>.+?)/(?P<objectname>.+?)$',
-             move_to_trash,
-             name="move_to_trash"),
+    url(r'^delete/(?P<pk>.+)$', upload_delete, name='jfu_delete'),
+    url(
+        r'^thumbnails/(?P<container>.+?)/(?P<objectname>.+?)$',
+        serve_thumbnail,
+        name="serve_thumbnail"),
+    url(
+        r'^move_to_trash/(?P<container>.+?)/(?P<objectname>.+?)$',
+        move_to_trash,
+        name="move_to_trash"),
     url(r'^trashview/(?P<account>.+?)/$', trashview, name="trashview"),
-    url(r'^delete_trash/(?P<account>.+?)/(?P<trashname>.+?)$', delete_trash,
-             name="delete_trash"),
-    url(r'^restore_trash/(?P<account>.+?)/(?P<trashname>.+?)$', restore_trash,
-             name="restore_trash"),
-    url(r'^restore_trash_collection/(?P<account>.+?)/(?P<trashname>.+?)$',
-             restore_trash_collection, name="restore_trash_collection"),
-    url(r'^move_collection_to_trash/(?P<container>.+?)/(?P<prefix>.+?)?$',
-             move_collection_to_trash,
-             name="move_collection_to_trash"),
-    url(r'^download_collection/(?P<container>.+?)/(?P<prefix>.+)?$',
-           download_collection, name="download_collection"),
-    url(r'^download_collection_nonrec/(?P<container>.+?)/(?P<prefix>.+)?$',
-            download_collection, {'non_recursive': True}, name="download_collection_nonrec"),
-    ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+    url(
+        r'^delete_trash/(?P<account>.+?)/(?P<trashname>.+?)$',
+        delete_trash,
+        name="delete_trash"),
+    url(
+        r'^restore_trash/(?P<account>.+?)/(?P<trashname>.+?)$',
+        restore_trash,
+        name="restore_trash"),
+    url(
+        r'^restore_trash_collection/(?P<account>.+?)/(?P<trashname>.+?)$',
+        restore_trash_collection,
+        name="restore_trash_collection"),
+    url(
+        r'^move_collection_to_trash/(?P<container>.+?)/(?P<prefix>.+?)?$',
+        move_collection_to_trash,
+        name="move_collection_to_trash"),
+    url(
+        r'^download_collection/(?P<container>.+?)/(?P<prefix>.+)?$',
+        download_collection,
+        name="download_collection"),
+    url(
+        r'^download_collection_nonrec/(?P<container>.+?)/(?P<prefix>.+)?$',
+        download_collection,
+        {'non_recursive': True},
+        name="download_collection_nonrec"),
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
