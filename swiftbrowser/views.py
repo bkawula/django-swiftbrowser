caf839b6bb643810792962d0bf34e126c0dd6db2
bartek kawula <b.kawula@utoronto.ca>
2015-04-29 17:10:36 -0400

    removed / commented out object thumbnails.

--- swiftbrowser/views.py
+++ swiftbrowser/views.py
@@ -635,7 +635,7 @@ def edit_acl(request, container):
         'base_url': base_url,
     }, context_instance=RequestContext(request))
 
-
+"""
 def serve_thumbnail(request, container, objectname):
     '''if request.session.get('username', '') == settings.THUMBNAIL_USER:
         return HttpResponseForbidden()'''
@@ -699,7 +699,7 @@ def serve_thumbnail(request, container, objectname):
         return HttpResponseServerError()
 
     return HttpResponse(image_data, mimetype=headers['content-type'])
-
+"""
 
 @require_POST
 def upload(request):
