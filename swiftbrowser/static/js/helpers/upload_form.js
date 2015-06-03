47bf7d188a32a29c2e1163d5f0fb1b7c867d2068
Peter Tran <pete.tran02@gmail.co>
2015-06-03 15:51:35 -0400

    Moved upload form js into separate file.

---
+++ swiftbrowser/static/js/helpers/upload_form.js
@@ -0,0 +1,197 @@
+$('#close-upload').on('click', function(){
+    $('#fileForm').foundation('reveal', 'close')
+});
+
+var filesuploaded = 0;
+var filesprocessed = 0;
+$( function() {
+    'use strict';
+
+    /*
+        Initialize the jQuery File Upload widget.
+
+        Default options are defined by the package,
+        however users may extend and define their
+        own options.
+    */
+
+    var formData = $('#fileupload').serializeArray();
+    formData.push(
+        {name:"redirect",value:""},
+        {name:"max_file_size",value:"{{ max_file_size }}"},
+        {name:"max_file_count",value:"{{ max_file_count }}"},
+        {name:"expires",value:"{{ expires }}"},
+        {name:"signature",value:"{{ signature }}"}
+    );
+
+    $('#fileupload').fileupload({
+        formData: formData,
+        url: '{{ swift_url }}',
+        method:'POST',
+        dataType:'text',
+        sequentialUploads: true,
+        recalculateProgress: false,
+        previewMaxWidth:100,
+        previewMaxHeight:100
+    })
+
+    .bind('fileuploadprocessstart', function (e) {
+        $('#preloadmsg').hide();
+        $('.fileupload-progress').show();
+        $('#start-upload').removeClass('disabled');
+        $('#cancel-upload').removeClass('disabled');
+    })
+
+    .bind('fileuploadprocessdone', function (e, data) {
+        filesprocessed = filesprocessed + 1;
+    })
+
+    .bind('fileuploadsend', function (e, data) {
+        Foundation.libs.reveal.settings.close_on_background_click = false;
+        $('#close-upload').off().addClass('disabled');
+
+        $.each(data.files, function (index, file) {
+
+            $('#fileForm').foundation({reveal : {close_on_background_click: false,close_on_esc:false}});
+        });
+    })
+
+    .bind('fileuploadfail', function (e, data) {
+        closeForm();
+    })
+
+    .bind('fileuploadcompleted', function (e, data) {
+        filesuploaded = filesuploaded + 1;
+        if (filesprocessed == filesuploaded) {
+            closeForm();
+        }
+    })
+    .addClass('fileupload-processing');
+
+});
+
+function closeForm() {
+    $('#fileForm').foundation({reveal : {close_on_background_click: true,close_on_esc:true}});
+    setTimeout(function(){
+        $('#fileForm').foundation('reveal', 'close');
+        setTimeout(function(){
+            $('#preloadmsg').show();
+            $('.fileupload-progress').hide();
+            $('#start-upload').addClass('disabled');
+            $('#cancel-upload').addClass('disabled');
+            $('#close-upload').on('click', function(){
+                $('#fileForm').foundation('reveal', 'close');
+            }).removeClass('disabled');
+        }, 250);
+    }, 250);
+    if(filesuploaded > 0) {
+        $('#messages').html(
+            '<div data-alert class="alert-box success header-alert">' +
+                '<div class="row" >' +
+                    '<div class="small-12 columns">' +
+                        '<strong>Success.</strong> ' + filesuploaded +
+                        ' files were added. <a href="#" class="close">&times;</a>' +
+                    '</div>' +
+                '</div>' +
+            '</div>'
+        );
+    }
+}
+
+/*
+    Modify jquery fileupload's onAdd method. This method recreates
+    the file object pointer and changes the name. The name change allows
+    users to drag and drop directories and those directories are
+    recreated on swift.
+
+*/
+$.blueimp.fileupload.prototype._onAdd = function (e, data) {
+    var that = this,
+        result = true,
+        options = $.extend({}, this.options, data),
+        files = data.files,
+        filesLength = files.length,
+        limit = options.limitMultiFileUploads,
+        limitSize = options.limitMultiFileUploadSize,
+        overhead = options.limitMultiFileUploadSizeOverhead,
+        batchSize = 0,
+        paramName = this._getParamName(options),
+        paramNameSet,
+        paramNameSlice,
+        fileSet,
+        i,
+        j = 0;
+    if (limitSize && (!filesLength || files[0].size === undefined)) {
+        limitSize = undefined;
+    }
+    if (!(options.singleFileUploads || limit || limitSize) ||
+            !this._isXHRUpload(options)) {
+        fileSet = [files];
+        paramNameSet = [paramName];
+    } else if (!(options.singleFileUploads || limitSize) && limit) {
+        fileSet = [];
+        paramNameSet = [];
+        for (i = 0; i < filesLength; i += limit) {
+            fileSet.push(files.slice(i, i + limit));
+            paramNameSlice = paramName.slice(i, i + limit);
+            if (!paramNameSlice.length) {
+                paramNameSlice = paramName;
+            }
+            paramNameSet.push(paramNameSlice);
+        }
+    } else if (!options.singleFileUploads && limitSize) {
+        fileSet = [];
+        paramNameSet = [];
+        for (i = 0; i < filesLength; i = i + 1) {
+            batchSize += files[i].size + overhead;
+            if (i + 1 === filesLength ||
+                    ((batchSize + files[i + 1].size + overhead) > limitSize) ||
+                    (limit && i + 1 - j >= limit)) {
+                fileSet.push(files.slice(j, i + 1));
+                paramNameSlice = paramName.slice(j, i + 1);
+                if (!paramNameSlice.length) {
+                    paramNameSlice = paramName;
+                }
+                paramNameSet.push(paramNameSlice);
+                j = i + 1;
+                batchSize = 0;
+            }
+        }
+    } else {
+        paramNameSet = paramName;
+    }
+    data.originalFiles = files;
+    $.each(fileSet || files, function (index, element) {
+
+        /****************** Swift browser edits here ******************/
+        /*
+            A new File object is created using "element". This new file
+            has a different ".name". This new name allows swift to
+            create pseudo folders. The end result is the file structure
+            that is dragged and dropped in swiftbrowser is preserved on
+            swift.
+        */
+
+        if (element.relativePath) {
+            var element = new File([element], element.relativePath + element.name, {type: "text/plain"});
+
+        } else if (element.webkitRelativePath){
+            console.log(typeof element.webkitRelativePath);
+            var element = new File([element], element.webkitRelativePath + element.name, {type: "text/plain"});
+
+        }
+        var newData = $.extend({}, data);
+        newData.files = fileSet ? element : [element];
+        newData.paramName = paramNameSet[index];
+        that._initResponseObject(newData);
+        that._initProgressObject(newData);
+        that._addConvenienceMethods(e, newData);
+        result = that._trigger(
+            'add',
+            $.Event('add', {delegatedEvent: e}),
+            newData
+        );
+        return result;
+    });
+    return result;
+}
