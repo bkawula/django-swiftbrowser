5bf992f2d6ff79e8f6f2fe968176ce9179b78a7f
Peter Tran <pete.tran02@gmail.com>
2016-03-01 16:28:53 -0500

    Updated to version 1.2.6

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup, find_packages
+from setuptools import setup
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
 
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-swiftbrowser',
-    version='1.2.5',
+    version='1.2.6',
     packages=['swiftbrowser'],
     include_package_data=True,
     license='Apache License (2.0)',
